050e20bc5ef6189b6b847bfd6bb799619f9cec0d
maccha-koubou <175473326+maccha-koubou@users.noreply.github.com>
2025-12-20 14:44:01 +0100

    Finished 9.1-9.3

---
+++ part9/bmiCalculator.ts
@@ -0,0 +1,17 @@
+const calculateBmi = (height: number, weight: number): string => {
+    const bmi = weight / (height * height / 10000);
+    if (bmi < 18.5) {
+        return 'Underweight'
+    } else if (bmi < 25) {
+        return 'Normal (healthy weight)'
+    } else if (bmi < 30) {
+        return 'Overweight'
+    } else {
+        return 'Obese'
+    }
+}
+
+const h: number = Number(process.argv[2])
+const w: number = Number(process.argv[3])
+
+console.log(calculateBmi(h, w));
---
+++ part9/exerciseCalculator.ts
@@ -0,0 +1,32 @@
+const calculateExercises = (timeEveryday: Array<number>, target: number) => {
+    const periodLength = timeEveryday.length
+    const trainingDays = timeEveryday.filter(day => day != 0).length
+    const average = timeEveryday.reduce((sum, day) => sum + day, 0) / periodLength
+    const success = average >= target
+    var rating
+    var ratingDescription
+    if (average >= target) {
+        rating = 3
+        ratingDescription = 'good'
+    } else if (average >= target / 2) {
+        rating = 2
+        ratingDescription = 'not too bad but could be better'
+    } else {
+        rating = 1
+        ratingDescription = 'could be better'
+    }
+
+    return { periodLength,
+        trainingDays,
+        success,
+        rating,
+        ratingDescription,
+        target,
+        average
+    }
+}
+
+const array: Array<number> = process.argv.slice(3).map(Number)
+const t: number = Number(process.argv[2])
+
+console.log(calculateExercises(array, t))
